ba438eaac11c08f4a957e3f8f3e4760a8e378c00
Nikolai Hedegaard Hansen <168433171+Niko-Hedegaard@users.noreply.github.com>
2024-05-13 14:34:36 +0200

    Safe point før jeg ændre i noget
    Jeg har tilføjet et JS script og lave nogle få ændringer til headeren i mobile version.

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+// Burgermenu function
+burgermenu = document.querySelector(".burgermenu");
+burgermenu.onclick = function () {
+    navbar = document.querySelector(".nav-bar");
+    navbar.classList.toggle("active");
+}
+
