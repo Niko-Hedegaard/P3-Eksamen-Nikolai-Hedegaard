cc1994b34fb1c7938c5c847dffd34bd11c04a12e
Nikolai Hedegaard Hansen <168433171+Niko-Hedegaard@users.noreply.github.com>
2024-05-14 21:41:55 +0200

    Tilføjet animationer og lidt tekst til sectionerne

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,31 @@
 // Burgermenu function
-burgermenu = document.querySelector(".burgermenu");
+burgermenu = document.querySelector('.burgermenu')
+navbar = document.querySelector('.nav-bar')
+burgermenuI = document.querySelector('.burgermenu i')
+header = document.querySelector('header')
+
 burgermenu.onclick = function () {
-    navbar = document.querySelector(".nav-bar");
-    navbar.classList.toggle("active");
+    navbar.classList.toggle('active');
+    const isOpen = navbar.classList.contains('active');
+
+    burgermenuI.classList = isOpen
+        ? 'fa-solid fa-xmark'
+        : 'fa-solid fa-bars'
 }
 
+
+function reveal() {
+    let text = document.querySelector('scroll-reveal')
+    for (var i = 0; i < text.length; i++) {
+        let windowHeight = window.innerHeight;
+        let elementTop = text[i].getBoundingClientRect().top;
+        let elementVisible = 300;
+
+        if (elementTop < windowHeight - elementVisible) {
+            text[i].classList.add('reveal')
+        }
+        else {
+            text[i].classList.remove('reveal')
+        }
+    }
+}
