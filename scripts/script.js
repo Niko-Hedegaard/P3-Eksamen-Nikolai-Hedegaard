182b2f2a6c34b798d7b42fc7cf83f66dbdee0304
Nikolai Hedegaard Hansen <168433171+Niko-Hedegaard@users.noreply.github.com>
2024-05-17 10:39:05 +0200

    Færdig hjemmeside u. korrektur
    Der er ikke læst korrektur på det her.

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,6 @@
 burgermenu = document.querySelector('.burgermenu')
 navbar = document.querySelector('.nav-bar')
 burgermenuI = document.querySelector('.burgermenu i')
-header = document.querySelector('header')
 
 burgermenu.onclick = function () {
     navbar.classList.toggle('active');
@@ -11,21 +10,4 @@ burgermenu.onclick = function () {
     burgermenuI.classList = isOpen
         ? 'fa-solid fa-xmark'
         : 'fa-solid fa-bars'
-}
-
-
-function reveal() {
-    let text = document.querySelector('scroll-reveal')
-    for (var i = 0; i < text.length; i++) {
-        let windowHeight = window.innerHeight;
-        let elementTop = text[i].getBoundingClientRect().top;
-        let elementVisible = 300;
-
-        if (elementTop < windowHeight - elementVisible) {
-            text[i].classList.add('reveal')
-        }
-        else {
-            text[i].classList.remove('reveal')
-        }
-    }
 }
